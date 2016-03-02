7cec8a3fc07b2368079dc4f18d602185ea8dba1b
lyyyuna <lyyyuna@gmail.com>
2016-03-02 20:57:33 +0800

    日志配置放入 logger.conf 中

--- wechat-asyncio/main.py
+++ wechat-asyncio/main.py
@@ -10,26 +10,9 @@ from Monitor import Monitor
 import auth
 
 import logging
-import logging.handlers
+import logging.config
 
-WXLOG_FILE = 'wx.log'
-MOLOG_FILE = 'monitor.log'
-
-handler = logging.handlers.RotatingFileHandler(WXLOG_FILE, maxBytes = 1024*1024, backupCount = 5) # 实例化handler
-fmt = '%(asctime)s <%(filename)s><%(lineno)d>: [%(levelname)s] - %(message)s'
-handler2 = logging.handlers.RotatingFileHandler(MOLOG_FILE, maxBytes = 1024*1024, backupCount = 5) # 实例化handler
-
-formatter = logging.Formatter(fmt)   #
-handler.setFormatter(formatter)      #
-handler2.setFormatter(formatter)
-logger = logging.getLogger('wx')    #
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
-logger1 = logging.getLogger('monitor')    #
-logger1.addHandler(handler2)
-logger1.setLevel(logging.DEBUG)
-
-
+logging.config.fileConfig("logger.conf")
 
 with aiohttp.ClientSession() as client, aiohttp.ClientSession() as rclient:
     wx = Wechat(client)
